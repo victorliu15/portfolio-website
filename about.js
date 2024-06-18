c18d0646d6ebb43b115be6f994245f3dff1135b2
Victor Liu <victorliu5000@gmail.com>
2024-06-18 11:45:42 -0400

    styling fixes

--- about.js
+++ about.js
@@ -4,11 +4,14 @@ document.addEventListener('DOMContentLoaded', function () {
         progressBars.forEach(bar => {
             if (bar.classList.contains('tennis')) {
                 bar.style.width = '75%';
-            } else if (bar.classList.contains('violin')) {
+            }
+            else if (bar.classList.contains('violin')) {
                 bar.style.width = '80%';
-            } else if (bar.classList.contains('swimming')) {
+            }
+            else if (bar.classList.contains('swimming')) {
                 bar.style.width = '50%';
-            } else if (bar.classList.contains('coding')) {
+            }
+            else if (bar.classList.contains('coding')) {
                 bar.style.width = '90%';
             }
         });

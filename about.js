6c7b1212a59a580f67734626a59910a1683eca71
Victor Liu <victorliu5000@gmail.com>
2024-06-18 11:41:41 -0400

    new background

---
+++ about.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const progressBars = document.querySelectorAll('.progress');
+    setTimeout(() => {
+        progressBars.forEach(bar => {
+            if (bar.classList.contains('tennis')) {
+                bar.style.width = '75%';
+            } else if (bar.classList.contains('violin')) {
+                bar.style.width = '80%';
+            } else if (bar.classList.contains('swimming')) {
+                bar.style.width = '50%';
+            } else if (bar.classList.contains('coding')) {
+                bar.style.width = '90%';
+            }
+        });
+    }, 100);
+});

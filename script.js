c60324655140da6162560615eeaa435aa2f14836
Victor Liu <victorliu5000@gmail.com>
2024-06-21 11:44:30 -0400

    contact me fix and resume added

---
+++ script.js
@@ -0,0 +1,33 @@
+function openPopup() {
+    document.getElementById("popup").classList.add("open-popup");
+    document.getElementById("mainContent").classList.add("blurred");
+}
+function closePopup() {
+    document.getElementById("popup").classList.remove("open-popup");
+    document.getElementById("mainContent").classList.remove("blurred");
+}
+function openResume() {
+    document.getElementById("resumeContainer").classList.add("show-resume");
+    document.getElementById("mainContent").classList.add("blurred");
+}
+function closeResume() {
+    document.getElementById("resumeContainer").classList.remove("show-resume");
+    document.getElementById("mainContent").classList.remove("blurred");
+}
+
+function copyToClipboard(elementId) {
+    var text = document.getElementById(elementId).innerText;
+    navigator.clipboard.writeText(text).then(function() {
+        showNotification();
+    }).catch(function(error) {
+        console.error('Error copying text: ', error);
+    });
+}
+
+function showNotification() {
+    var notification = document.getElementById('notification');
+    notification.classList.add('show');
+    setTimeout(function() {
+        notification.classList.remove('show');
+    }, 2000);
+}
